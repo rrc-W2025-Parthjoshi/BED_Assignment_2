c1949a584c432ec337f0c6bf172120654011bd09
Parth Joshi <pjoshi32@academic.RRC.CA>
2026-02-06 15:37:44 -0600

    added health check endpoint

--- src/app.ts
+++ src/app.ts
@@ -1,13 +1,12 @@
 import express, { Express } from "express";
 import morgan from "morgan";
+import healthRoutes from "./api/v1/routes/healthRoutes";
 
 const app: Express = express();
 
-// Use Morgan for HTTP request logging
 app.use(morgan("combined"));
-
-// Parse JSON bodies
 app.use(express.json());
 
-// Export the app
+app.use("/api/v1", healthRoutes);
+
 export default app;

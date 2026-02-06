ab4aecb4bf784cb2cdb64dc98feea1618642ebcc
Parth Joshi <pjoshi32@academic.RRC.CA>
2026-02-06 17:41:30 -0600

    Added createTicket service function

--- src/api/v1/services/ticketService.ts
+++ src/api/v1/services/ticketService.ts
@@ -1,3 +1,5 @@
+import { tickets } from "../../../data/tickets";
+
 export type TicketPriority = "critical" | "high" | "medium" | "low";
 export type TicketStatus = "open" | "in-progress" | "resolved";
 
@@ -10,3 +12,24 @@ export interface Ticket {
   createdAt: string;
   updatedAt: string;
 }
+
+export const createTicket = (
+  title: string,
+  description: string,
+  priority: TicketPriority
+): Ticket => {
+  const now = new Date().toISOString();
+
+  const newTicket: Ticket = {
+    id: tickets.length + 1,
+    title,
+    description,
+    priority,
+    status: "open",
+    createdAt: now,
+    updatedAt: now,
+  };
+
+  tickets.push(newTicket);
+  return newTicket;
+};

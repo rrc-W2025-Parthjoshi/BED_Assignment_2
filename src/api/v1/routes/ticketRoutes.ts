8d702ba334d0483a3551ff480eb480b50a8a8344
Parth Joshi <pjoshi32@academic.RRC.CA>
2026-02-07 01:05:37 -0600

    add sample ticket data

--- src/api/v1/routes/ticketRoutes.ts
+++ src/api/v1/routes/ticketRoutes.ts
@@ -1,10 +1,11 @@
 import { Router } from "express";
-import { createTicketHandler,getAllTicketsHandler,getTicketByIdHandler } from "../controllers/ticketController";
+import { createTicketHandler,getAllTicketsHandler,getTicketByIdHandler,updateTicketHandler } from "../controllers/ticketController";
 
 const router = Router();
 
 router.post("/tickets", createTicketHandler);
 router.get("/tickets", getAllTicketsHandler);
 router.get("/tickets/:id", getTicketByIdHandler);
+router.put("/tickets/:id", updateTicketHandler);
 
 export default router;

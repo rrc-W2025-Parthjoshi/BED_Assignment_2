f11db7e8b916cc1ece8ad0386c4f101e78e8bb69
Parth Joshi <pjoshi32@academic.RRC.CA>
2026-02-06 20:52:38 -0600

    Added route in app.ts for ticketroutes

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,7 @@
 import express, { Express } from "express";
 import morgan from "morgan";
 import healthRoutes from "./api/v1/routes/healthRoutes";
+import ticketRoutes from "./api/v1/routes/ticketRoutes";
 
 const app: Express = express();
 
@@ -8,5 +9,6 @@ app.use(morgan("combined"));
 app.use(express.json());
 
 app.use("/api/v1", healthRoutes);
+app.use("/api/v1", ticketRoutes);
 
 export default app;
